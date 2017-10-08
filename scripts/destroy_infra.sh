a1f55fd037a702c8b0240a1ecf7ed3830920922f
Gary A. Stafford <garystafford@rochester.rr.com>
2017-10-08 19:28:48 -0400

    Update remote state commands

--- scripts/destroy_infra.sh
+++ scripts/destroy_infra.sh
@@ -9,12 +9,6 @@ echo "*** Destroying AWS infrastructure ***"
 
 cd ../tf_env_aws/
 
-terraform remote config \
-  -backend=s3 \
-  -backend-config="bucket=tf-remote-state-gstafford" \
-  -backend-config="key=terraform_consul.tfstate" \
-  -backend-config="region=us-east-1"
-
 terraform destroy -force -no-color
 
 echo " "
