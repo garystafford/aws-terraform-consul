f5f6dbfd75ba7fa32f5f191dfd0720a1ac7b0ab1
Gary A. Stafford <garystafford@rochester.rr.com>
2017-10-08 17:03:10 -0400

    Update remote state commands

--- scripts/provision_infra.sh
+++ scripts/provision_infra.sh
@@ -7,12 +7,12 @@ set -ex
 echo " "
 echo "*** Building AWS Consul infrastructure ***"
 
+cd ../tf_env_aws
 terraform init \
   -backend-config="bucket=tf-remote-state-gstafford" \
   -backend-config="key=terraform_adop.tfstate" \
   -backend-config="region=us-east-1" \
   -backend=true -force-copy -get=true -input=false
-
 
 terraform plan -no-color
 terraform apply -no-color
